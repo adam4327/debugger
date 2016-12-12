ec97e8003ed4a9fc44ff7c2fc07174bd442df18c
unknown <c3427357@JG109-P15.aet.leedsbeckett.ac.uk>
2016-12-12 12:17:32 +0000

    search

--- Debugger/search.cs
+++ Debugger/search.cs
@@ -1,77 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
-namespace Debugger
-{
-    public partial class search : Form
+    namespace Debugger
     {
-        SqlConnection mySqlConnection;
-        public search()
+        public partial class search : Form
         {
-            InitializeComponent();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-/// <summary>
-/// display the details after the search
-/// </summary>
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
+            SqlConnection mySqlConnection;
+            public search()
             {
-                mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");
+                InitializeComponent();
+            }
 
-                mySqlConnection.Open();
-                //search query % (anything before or after)
-                String report = ("SELECT * FROM [errors] WHERE description LIKE '%" + textBox1.Text + "%' OR error LIKE '%" + textBox1.Text + "%' OR Id LIKE '%" + textBox1.Text + "%';");
-                SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
-                reportCommand.ExecuteNonQuery();
+            private void textBox1_TextChanged(object sender, EventArgs e)
+            {
 
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection);
+            }
+    /// <summary>
+    /// display the details after the search
+    /// </summary>
 
-                da.Fill(dt);
-                dataGridView1.DataSource = dt;
+            private void button1_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");
+
+                    mySqlConnection.Open();
+                    //search query % (anything before or after)
+                    String report = ("SELECT * FROM [errors] WHERE description LIKE '%" + textBox1.Text + "%' OR error LIKE '%" + textBox1.Text + "%' OR Id LIKE '%" + textBox1.Text + "%';");
+                    SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
+                    reportCommand.ExecuteNonQuery();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection);
+
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
                 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed due to: " + ex.Message);
+                }
             }
-            catch (Exception ex)
+            /// <summary>
+            /// user can click on data
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
             {
-                MessageBox.Show("Failed due to: " + ex.Message);
+
+                if (dataGridView1.SelectedCells.Count > 0)
+                {
+                    int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+
+                    string a = Convert.ToString(selectedRow.Cells["Id"].Value);
+
+                    MessageBox.Show(a);
+
+                }
+                //MessageBox.Show(dataGridView1.SelectedCells[0].Value.ToString());
             }
+
         }
-        /// <summary>
-        /// user can click on data
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-            if (dataGridView1.SelectedCells.Count > 0)
-            {
-                int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
-
-                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
-
-                string a = Convert.ToString(selectedRow.Cells["Id"].Value);
-
-                MessageBox.Show(a);
-
-            }
-            //MessageBox.Show(dataGridView1.SelectedCells[0].Value.ToString());
-        }
-
     }
-}
