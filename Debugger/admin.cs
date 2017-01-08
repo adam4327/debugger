91540a7e91a9842679d82d0b651a14213da0e7fa
unknown <c3427357@JG109-P15.aet.leedsbeckett.ac.uk>
2017-01-08 18:42:14 +0000

    Code clean up

--- Debugger/admin.cs
+++ Debugger/admin.cs
@@ -1,96 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
-namespace Debugger
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    public partial class admin : Form
+    namespace Debugger
     {
-        SqlConnection mySqlConnection;
-        public admin(string admin)
-        {
-            InitializeComponent();
-            label2.Text = admin; // sets label to admin
-
-
-        }
-
         /// <summary>
-        /// Deletes user specified by the admin, then the table is updated
+        /// 
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
+        public partial class admin : Form
         {
-            try
+            SqlConnection mySqlConnection;
+            public admin(string admin)
             {
-                mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");// new sql connection
-
-                mySqlConnection.Open();
-                String delete = ("DELETE FROM [user] WHERE username = '" + textBox1.Text + "'; "); // delete user on what the admin types in
-                SqlCommand deleteCommand = new SqlCommand(delete, mySqlConnection);
-                deleteCommand.ExecuteNonQuery();
-
-
-                //refresh the table with the user deleted
-                String report = ("SELECT * FROM [user]");
-                SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
-                reportCommand.ExecuteNonQuery();
-
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection);
-
-                da.Fill(dt);
-                dataGridView1.DataSource = dt;
+                InitializeComponent();
+                label2.Text = admin; // sets label to admin
 
 
             }
-            catch (Exception ex)
+
+            /// <summary>
+            /// Deletes user specified by the admin, then the table is updated
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void button1_Click(object sender, EventArgs e)
             {
-                MessageBox.Show("Failed due to: " + ex.Message);
+                try
+                {
+                    mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");// new sql connection
+
+                    mySqlConnection.Open();
+                    String delete = ("DELETE FROM [user] WHERE username = '" + textBox1.Text + "'; "); // delete user on what the admin types in
+                    SqlCommand deleteCommand = new SqlCommand(delete, mySqlConnection);
+                    deleteCommand.ExecuteNonQuery();
+
+
+                    //refresh the table with the user deleted
+                    String report = ("SELECT * FROM [user]");
+                    SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
+                    reportCommand.ExecuteNonQuery();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection);
+
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed due to: " + ex.Message);
+                }
+
             }
 
-        }
-
-        /// <summary>
-        /// load the table full of users
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void admin_Load(object sender, EventArgs e)
-        {
-            try
+            /// <summary>
+            /// load the table full of users
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void admin_Load(object sender, EventArgs e)
             {
-                mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");
+                try
+                {
+                    mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\computing3\adv_softwareEng\Debugger\Debugger\test.mdf;Integrated Security=True;Connect Timeout=30");
 
-                mySqlConnection.Open();
-                String report = ("SELECT * FROM [user]"); // select all the users
-                SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
-                reportCommand.ExecuteNonQuery();
+                    mySqlConnection.Open();
+                    String report = ("SELECT * FROM [user]"); // select all the users
+                    SqlCommand reportCommand = new SqlCommand(report, mySqlConnection);
+                    reportCommand.ExecuteNonQuery();
 
-                DataTable dt = new DataTable(); // create new table
-                SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection); //fills in the data
+                    DataTable dt = new DataTable(); // create new table
+                    SqlDataAdapter da = new SqlDataAdapter(report, mySqlConnection); //fills in the data
 
-                // fill table
-                da.Fill(dt);
-                dataGridView1.DataSource = dt;
+                    // fill table
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed due to: " + ex.Message); // any errors are displayed in a message box
+                }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed due to: " + ex.Message); // any errors are displayed in a message box
-            }
-        }
     
+        }
     }
-}
